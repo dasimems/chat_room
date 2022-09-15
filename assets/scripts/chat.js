04bae9751b2df8d506febd45319d7c2119458f05
dasimems <isaacseun63@gmail.com>
2022-09-15 05:46:30 -0400

    did the image show box and the other chat form content attribute

--- assets/scripts/chat.js
+++ assets/scripts/chat.js
@@ -1,13 +1,34 @@
 "use strict";
 
 var docProps = {
-    menuOpened: false
+    menuOpened: false,
+    otherChatOptionOpened: false,
+    mediaBoxOpened: false
 }
 
 window.addEventListener("load", ()=>{
-    var headerButton = document.querySelector(".header-button");
+    var headerButton = document.querySelector(".header-button"),
+        otherThingsButton = document.getElementById("add-item"),
+        mediaImage = document.querySelectorAll(".media-content-cards .media .media-image img"),
+        showCaseImageCloseButton = document.querySelector(".image-show-case .image-show-case-header #close-image-show-case-btn"),
+        counterContainer = document.querySelector(".image-show-case .image-show-case-footer .stats .total-count");
 
-    headerButton.addEventListener("click", menu)
+    headerButton.addEventListener("click", menu);
+    otherThingsButton.addEventListener("click", openContent);
+    showCaseImageCloseButton.addEventListener("click", closeImageShowCase)
+
+    counterContainer.innerText = mediaImage.length;
+
+    mediaImage.forEach((images, index)=>{
+        images.addEventListener("click", (e)=>{
+            // console.log(e.target.src);
+            showImageShowCase(e.target.src, index);
+        })
+    })
+
+
+
+
 })
 
 function menu(){
@@ -133,3 +154,123 @@ function closeMenu(){
     }, 300)
 
 }
+
+function openContent(){
+    var addItemButton = document.getElementById("add-item"),
+        defaultButton = document.querySelectorAll(".default-chat-content"),
+        otherButton = document.querySelectorAll(".other-chat-content");
+
+    if(docProps.otherChatOptionOpened){
+
+        addItemButton.style.transform = "rotateZ(0deg)";
+        docProps.otherChatOptionOpened = false;
+
+        otherButton.forEach((element) =>{
+                element.style.opacity = "0";
+        })
+
+        setTimeout(()=>{
+            otherButton.forEach(element =>{
+                element.style.display = "none";
+                element.style.animationName = "";
+                element.style.animationDelay = "0s";
+            })
+
+            defaultButton.forEach(element => {
+                element.style.display = "block";
+            })
+    
+        }, 500)
+
+    }else{
+        
+        addItemButton.style.transform = "rotateZ(-315deg)";
+        docProps.otherChatOptionOpened = true;
+
+        defaultButton.forEach(element => {
+            element.style.display = "none";
+        })
+
+        
+        otherButton.forEach(element =>{
+            element.style.display = "block";
+        })
+
+        setTimeout(()=>{
+
+            otherButton.forEach((element, index) =>{
+                element.style.animationName = "pushAnimate";
+                element.style.animationDelay = (index * 0.1) + "s";
+                element.style.opacity = "1";
+            })
+
+        }, 10)
+
+    }
+
+}
+
+function showImageShowCase(imageDir, count){
+    var showCaseContainer = document.querySelector(".image-show-case"),
+        showCaseImage = document.querySelector(".image-show-case .image-show-case-content img"),
+        showCaseLoader = document.querySelector(".image-show-case .image-show-case-content .loader"),
+        counterContainer = document.querySelector(".image-show-case .image-show-case-footer .stats .present-count");
+
+    showCaseContainer.style.display = "block";
+    counterContainer.innerText = (count + 1);
+    
+    setTimeout(()=>{
+        showCaseContainer.style.opacity = "1"
+        showCaseImage.src = imageDir;
+        hideChat()
+
+        showCaseImage.addEventListener("load", ()=>{
+            // console.log("worked");
+            showCaseLoader.style.opacity = "0";
+            showCaseImage.style.opacity = "1";
+        })
+    }, 50)
+
+
+}
+
+function closeImageShowCase(){
+
+    var showCaseContainer = document.querySelector(".image-show-case"),
+        showCaseImage = document.querySelector(".image-show-case .image-show-case-content img"),
+        showCaseLoader = document.querySelector(".image-show-case .image-show-case-content .loader");
+
+        showCaseContainer.style.opacity = "0";
+        showChat();
+
+        setTimeout(()=>{
+            showCaseContainer.style.display = "none";
+            showCaseImage.src = "",
+            showCaseLoader.style.opacity = "1";
+            showCaseImage.style.opacity = "0";
+        }, 1000)
+
+}
+
+function hideChat(){
+    var chatHeader = document.querySelector(".chats .contact-header"),
+        chatContent = document.querySelector(".chats .chat-content"),
+        chatForm = document.querySelector(".chats .chat-form");
+
+        chatHeader.style.opacity = "0";
+        chatContent.style.opacity = "0";
+        chatForm.style.opacity = "0";
+}
+
+function showChat(){
+
+    var chatHeader = document.querySelector(".chats .contact-header"),
+        chatContent = document.querySelector(".chats .chat-content"),
+        chatForm = document.querySelector(".chats .chat-form");
+
+        chatHeader.style.opacity = "1";
+        chatContent.style.opacity = "1";
+        chatForm.style.opacity = "1";
+
+}
+
